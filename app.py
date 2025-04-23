9de2b02bc7cb5d08f489835559d4c2e2d79cd45b
Dongyin Zhuo <dongyin.zhuo@gbzdy.de>
2025-04-23 13:39:36 +0200

    fin, I guess...

--- app.py
+++ app.py
@@ -1,34 +1,49 @@
 from flask import Flask, jsonify, request, render_template
 import tensorflow as tf
 import numpy as np
+import secrets
+import joblib
+from losses import custom_loss
+
 app = Flask(__name__)
 
-model = tf.keras.models.load_model("BreathingModel.h5")
+model = tf.keras.models.load_model("BreathingModel_2.keras", custom_objects={"custom_loss": custom_loss})
+scaler = joblib.load('scaler.save')
+output_scaler = joblib.load('y_scaler.save')
 
 def preprocess_input(raw_data):
-    goal = int(raw_data['goal'])
-    intensity_function = int(raw_data['intensity_function']) / 100.0
-    intensity_time = int(raw_data['intensity_time']) / 100.0
-    return np.array([goal, intensity_function, intensity_time])
+    intensity_function = int(raw_data['intensity_function'])
+    intensity_time = int(raw_data['intensity_time'])
+    age_group = int(raw_data['age_group'])
+    gender = int(raw_data['gender'])
+    return scaler.transform(np.array([[intensity_function, intensity_time, age_group, gender]]))
 
 
 @app.route('/recommend', methods=['POST'])
 def recommend():
     data = request.json
     features = preprocess_input(data) #normalization
-    prediction = model.predict(features.reshape(1, -1))[0]
-    output_min = np.array([746, 3, 5, 4])  # tick_length, inhale, exhale, repetition
-    output_max = np.array([1250, 9, 10, 9])
-    tick_length, inhale,  exhale, repetition = prediction * (output_max - output_min) + output_min
+    prediction = model.predict(features)
+    inhale,  exhale, repetition, r, s = output_scaler.inverse_transform(prediction.reshape(1, -1))[0]
+    mode = secrets.randbelow(100) % 2
+    if mode == 1:
+        inhale = 5
+        exhale = 5
+        repetition = 6
     return jsonify({
-        "inhale": int(inhale * tick_length),
-        "exhale": int(exhale * tick_length),
-        "repetition": int(repetition)
+        "inhale": int(inhale * 1000),
+        "exhale": int(exhale * 1000),
+        "repetition": int(repetition),
+        "mode": int(mode)
     })
+
+@app.route('/ai', methods=['GET'])
+def ai_breath():
+    return render_template("index_copy.html")
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001)
